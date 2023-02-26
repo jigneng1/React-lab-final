4dce9b6bf9409b656d4f84d1ec6ba54daea461cd
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-02-26 17:08:52 +0700

    finish

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assets/neng.jpg" alt="profile"/>
+            <h1 className="name">Neng Sitiporn</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
